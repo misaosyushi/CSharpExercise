163945fe0a24bfe1423ec37411cb5d7b544310e9
misaosyushi <tujitamisaki@gmail.com>
2021-04-20 08:20:33 +0900

    add ex2

---
+++ Ex2/MyClass.cs
@@ -0,0 +1,14 @@
+namespace Ex2
+{
+    public class MyClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    struct MyStruct
+    {
+        public int X { get; set; }
+        public int Y { get; set; } 
+    }
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj1 = new MyClass {X = 10, Y = 20};
+            var obj2 = new MyStruct {X = 100, Y = 200};
+            PrintObjects(obj1, obj2);
+        }
+
+        static void PrintObjects(MyClass obj1, MyStruct obj2)
+        {
+            obj1.X *= 2;
+            obj1.Y *= 2;
+            obj2.X *= 2;
+            obj2.Y *= 2;
+            Console.WriteLine("MyClass: {0}, {1}", obj1.X, obj1.Y);
+            Console.WriteLine("MyClass: {0}, {1}", obj2.X, obj2.Y);
+        }
+    }
+}
