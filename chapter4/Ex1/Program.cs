2dfd2e4b48935eff43184ad976940eda9e1b2198
misaosyushi <tujitamisaki@gmail.com>
2021-04-21 08:37:40 +0900

    add chapter4 ex1

---
+++ chapter4/Ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class YearMonth
+    {
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public bool Is21Century
+        {
+            get { return 2001 <= Year && Year <= 2100; }
+        }
+
+        public YearMonth AddOneMonth()
+        {
+            if (this.Month == 12)
+            {
+                return new YearMonth(this.Year + 1, 1);
+            }
+
+            return new YearMonth(this.Year, this.Month + 1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
