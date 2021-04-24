631054361f81e9a18139ae347a25dc902a159983
misaosyushi <tujitamisaki@gmail.com>
2021-04-24 15:36:52 +0900

    add chapter7 ex1

---
+++ chapter7/Ex1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Ex1_1(text);
+            Console.WriteLine("-----");
+            Ex1_2(text);
+        }
+
+        static void Ex1_1(string text)
+        {
+            var dict = new Dictionary<Char, int>();
+            foreach (var c in text)
+            {
+                var uc = char.ToUpper(c);
+                if ('A' <= uc && uc <= 'Z')
+                {
+                    if (dict.ContainsKey(uc))
+                        dict[uc]++;
+                    else
+                        dict[uc] = 1;
+                }
+            }
+            foreach (var keyValuePair in dict.OrderBy(s => s.Key))
+            {
+                Console.WriteLine("{0} : {1}", keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+        
+        static void Ex1_2(string text)
+        {
+            var dict = new SortedDictionary<Char, int>();
+            foreach (var c in text)
+            {
+                var uc = char.ToUpper(c);
+                if ('A' <= uc && uc <= 'Z')
+                {
+                    if (dict.ContainsKey(uc))
+                        dict[uc]++;
+                    else
+                        dict[uc] = 1;
+                }
+            }
+            foreach (var keyValuePair in dict)
+            {
+                Console.WriteLine("{0} : {1}", keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+    }
+}
