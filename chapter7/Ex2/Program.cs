ac33855142bee925967211ba217718d05e0d9f2b
misaosyushi <tujitamisaki@gmail.com>
2021-04-24 15:58:37 +0900

    add chapter7 ex2

---
+++ chapter7/Ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abbreviations = new Abbreviations();
+            abbreviations.Add("test1", "テスト1");
+            abbreviations.Add("test2", "テスト2");
+            Ex2_3(abbreviations);
+            Console.WriteLine("-----");
+            Ex2_4(abbreviations);
+        }
+
+        static void Ex2_3(Abbreviations abbreviations)
+        {
+            Console.WriteLine(abbreviations.Count);
+            abbreviations.Remove("test1");
+            foreach (var keyValuePair in abbreviations)
+            {
+                Console.WriteLine("{0}={1}", keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+        
+        static void Ex2_4(Abbreviations abbreviations)
+        {
+            var res = abbreviations.Where(a => a.Key.Length == 3);
+            foreach (var keyValuePair in res)
+            {
+                Console.WriteLine("{0}={1}", keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+    }
+}
