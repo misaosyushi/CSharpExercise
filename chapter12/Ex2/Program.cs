2c74aef10516bf5ab15b16773580d435c234763c
misaosyushi <tujitamisaki@gmail.com>
2021-04-24 16:20:21 +0900

    add chapter12 ex2

---
+++ chapter12/Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var novelist = new Novelist
+            {
+                Name = "test", Birth = new DateTime(2021, 4, 24), Masterpieces = new[] {"test"}
+            };
+            Exercise2_2(novelist, "novelist.json");
+            Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine();
+        }
+
+        static void Exercise2_2(Novelist novelist, string outfile)
+        {
+            using (var stream = new FileStream(outfile, FileMode.Create,
+                FileAccess.Write))
+            {
+                var serializer = new DataContractJsonSerializer(novelist.GetType(),
+                    new DataContractJsonSerializerSettings
+                    {
+                        DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                    });
+                serializer.WriteObject(stream, novelist);
+            }
+        }
+    }
+
+    [DataContract]
+    class Novelist
+    {
+        [DataMember(Name = "name")] public string Name { get; set; }
+        [DataMember(Name = "birth")] public DateTime Birth { get; set; }
+        [DataMember(Name = "masterpieces")] public string[] Masterpieces { get; set; }
+    }
+}
