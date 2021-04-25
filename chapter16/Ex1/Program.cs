9e7b113689316c133483de9f675eebf176a3c887
misaosyushi <tujitamisaki@gmail.com>
2021-04-26 08:46:27 +0900

    add chapter16 ex1

---
+++ chapter16/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex1_1();
+        }
+
+        static async void Ex1_1()
+        {
+            using (StreamReader reader = File.OpenText("test.txt"))
+            {
+                Console.WriteLine("Open Text.");
+                var result = await reader.ReadLineAsync();
+                Console.WriteLine("First line contains: " + result);
+            }
+        }
+    }
+}
